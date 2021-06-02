17eb5a77ab092d0f716cb779b2832b5bf2951a0b
DESKTOP-OKR5BMP\etoos <a02t02@gmail.com>
2021-06-02 18:27:51 +0900

    Add getStaticProps, getServerSideProps

--- prepare/back/routes/user.js
+++ prepare/back/routes/user.js
@@ -40,6 +40,42 @@ router.get('/', async (req, res, next) => {  // GET /user
   }
 });
 
+router.get('/:userId', async (req, res, next) => { // GET /user/3
+  try {
+    const fullUserWithoutPassword = await User.findOne({
+      where: { id: req.params.userId },
+      attributes: {
+        exclude: ['password']
+      },
+      include: [{
+        model: Post,
+        attributes: ['id'],
+      }, {
+        model: User,
+        as: 'Followings',
+        attributes: ['id'],
+      }, {
+        model: User,
+        as: 'Followers',
+        attributes: ['id'],
+      }]
+    })
+    if (fullUserWithoutPassword) {
+      // 시퀄아이저 데잍는 toJSON을 통해 쓸수 있는 데이터로 변환시킨다
+      const data = fullUserWithoutPassword.toJSON();
+      data.Posts = data.Posts.length; // 개인 정보 침해 예방
+      data.Followings = data.Followings.length;
+      data.Followers = data.Followers.length;
+      res.status(200).json(data);
+    } else {
+      res.status(404).json('존재하지 않는 사용자입니다.');
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     // passport 전략의 리턴값이 전달된다..
